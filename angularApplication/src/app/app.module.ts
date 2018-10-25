e70c4c21c21ca3e70cf4a3a496cbd2b2837663b6
Jaz <jaz94salas@gmail.com>
2018-10-25 09:04:21 -0500

    Commit previo descarga

--- angularApplication/src/app/app.module.ts
+++ angularApplication/src/app/app.module.ts
@@ -4,6 +4,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { AppComponent } from './app.component';
 
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatInputModule,
+    
+     FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
